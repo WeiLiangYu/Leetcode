a86b89e2e28c078b9d57610ebccd265aa9a520e2
WeiLiangYu <72431578+WeiLiangYu@users.noreply.github.com>
2023-11-28 02:09:29 +0000

    Sync LeetCode submission - Design Linked List (python3)

--- my-folder/problems/design_linked_list/solution.py
+++ my-folder/problems/design_linked_list/solution.py
@@ -1,16 +1,14 @@
 class Node:
-    def __init__(self, val, next=None):
+    def __init__(self, val, next):
         self.val = val
         self.next = next
-
 class MyLinkedList:
-
     def __init__(self):
         self.head = None
         self.size = 0
 
     def get(self, index: int) -> int:
-        if self.size <= index or index < 0:
+        if index < 0 or index >= self.size :
             return -1
 
         currNode = self.head
@@ -23,45 +21,45 @@ class MyLinkedList:
         self.size += 1
 
     def addAtTail(self, val: int) -> None:
-        currNode = self.head
-        tailNode = Node(val, next = None)
         if self.size == 0:
-            self.head = tailNode
+            self.head = Node(val, None)
         else:
-            while currNode.next:
+            currNode = self.head
+            while(currNode.next):
                 currNode = currNode.next
-            currNode.next = tailNode
+            currNode.next = Node(val, None)
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        """*arg = (index, val)"""
-        if self.size < index:
+        if index > self.size:
             return
-        currNode = self.head
-        
         if index == 0:
-            self.addAtHead(val)
+            self.head = Node(val, self.head)
         else:
-            # 遞歸到指定的index-1的Node
-            # 新Node後接index的Node
-            # index-1的Node後接新Node
-            for _ in range(index - 1):
+            currNode = self.head
+            for _ in range(index-1):
                 currNode = currNode.next
-
             addNode = Node(val, currNode.next)
             currNode.next = addNode
-
         self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size:
+        if index > self.size-1 or index < 0:
             return
+
         currNode = self.head
         if index == 0:
             self.head = self.head.next
         else:
-            for _ in range(index - 1):
+            for _ in range(index-1):
                 currNode = currNode.next
             currNode.next = currNode.next.next
-
         self.size -= 1
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
